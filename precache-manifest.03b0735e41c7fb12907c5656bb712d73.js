519d64863f8faf10839f1ff42cb5d5abfacb0671
kolawolejohn <kolawolejohn@users.noreply.github.com>
2021-07-01 15:02:49 +0000

    deploy: e6c0def70fc19084185bde8f6e4e82064a529b17

---
+++ precache-manifest.03b0735e41c7fb12907c5656bb712d73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bb99b57838fb3677aefcf3cc1ec25fa",
+    "url": "https://kolawolejohn.github.io/my-task-management-frontend/index.html"
+  },
+  {
+    "revision": "9393157d37ec11e7a10c",
+    "url": "https://kolawolejohn.github.io/my-task-management-frontend/static/css/main.8e1ae80c.chunk.css"
+  },
+  {
+    "revision": "a41daf5ca66a20d9c817",
+    "url": "https://kolawolejohn.github.io/my-task-management-frontend/static/js/2.95a8dbab.chunk.js"
+  },
+  {
+    "revision": "9393157d37ec11e7a10c",
+    "url": "https://kolawolejohn.github.io/my-task-management-frontend/static/js/main.783fe67e.chunk.js"
+  },
+  {
+    "revision": "b33b4c3f5c9d87a78d6c",
+    "url": "https://kolawolejohn.github.io/my-task-management-frontend/static/js/runtime~main.3e676b9b.js"
+  }
+]);
